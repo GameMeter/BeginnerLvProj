031d1c8a15c1bb5c4db6c3f0d26ba83c6cc9e81b
GameMeter <127404526+GameMeter@users.noreply.github.com>
2023-03-09 20:25:43 +0300

    Credit check
    Simple Level

---
+++ Good Credit.py
@@ -0,0 +1,14 @@
+House = 1000000
+Credit_Good = True
+buyer = input("Hello, what is your name?")
+
+if Credit_Good is True:
+    Downpayment = House * 0.1
+elif Credit_Good is False:
+    Downpayment = House * 0.2
+else:
+    print("Im not selling this house to you!")
+    exit()
+
+
+print(f"For this house worth {str(House)}$ you need to put down {str(int(Downpayment))}$ as downpayment")
