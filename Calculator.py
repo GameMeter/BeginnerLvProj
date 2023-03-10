8f9cdb65863940fd76f37b00ae8b0f5403e7fbbd
GameMeter <127404526+GameMeter@users.noreply.github.com>
2023-03-10 07:12:29 +0300

    Test file
    L1 #1, Just prints numbers

---
+++ Calculator.py
@@ -0,0 +1,25 @@
+num = 0 
+
+def whileloop():
+    while True:
+        global num
+        num += 1
+        Buzz = False
+        Bash = False
+        numcheck1 = num % 3
+        numcheck2 = num % 5
+
+        if numcheck1 == 0:
+            Buzz = True
+        if numcheck2 == 0:
+            Bash = True
+        if Buzz and Bash:
+            print(f"Jackpot {num}")
+        elif Buzz:
+            print(f"Buzz {num}")
+        elif Bash:
+            print(f"Bash {num}")
+        else:
+            print(num)
+
+whileloop()
